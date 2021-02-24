b0972206646d5ade072dc49e10a1b1a35eaea7c7
vibhorgoyal <goyal1992.vibhor@gmail.com>
2021-02-24 22:39:46 +0530

    Made changes in header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,53 +1,41 @@
 import React from 'react';
 
+import {signInWithGoogle} from '../../firebase/firebase.utils';
 import {Link} from 'react-router-dom';
 import {auth} from '../../firebase/firebase.utils';
 
 
+const Pageheader = ({currentUser}) => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-md nav-bar-background navbar-dark sticky-top">
 
-
-
-
-const Pageheader = ({currentUser})=>{
-    return(
-     <div>
-        <nav className="navbar navbar-expand-md bg-secondary navbar-dark sticky-top">
-        
-        <Link  className="navbar-brand"  to='/'> Home</Link>
-        
-           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            
-            <div className="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
-          
-          <ul className="navbar-nav ">
-              
-              <li className="nav-item ">
-              <Link className="nav-link" to='/'>About Me</Link>
-            </li>
-            <li className="nav-item ">
-              <Link className="nav-link" to='/buildresume'>Build My Resume</Link>
-            </li>
-            
-           
-              {
-                
-                currentUser?
-                <Link className="nav-link" onClick={()=>auth.signOut()}>Log Out </Link>
-                :
-                <Link className="nav-link" to='/signin'>Log In </Link>
-              
-              }
-              </ul>
-          </div>  
-        
-        </nav>
-        
-  
+                <Link className="navbar-brand" to='/'>
+                    Build My Resume
+                </Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#collapsibleNavbar">
+                    <span className="navbar-toggler-icon"/>
+                </button>
+                <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul className="navabr-header navbar-nav ">
+                        <li className="nav-item ">
+                            <Link className="nav-link" to='/buildresume'>Resume Builder</Link>
+                        </li>
+                    </ul>
+                    <ul className="nav navbar-nav ml-auto">
+                        <li>
+                            {
+                                currentUser ?
+                                    <a className="nav-link pull-right" onClick={() => auth.signOut()}>Log Out</a>
+                                    :
+                                    <button type="button" className="btn btn-primary" onClick={signInWithGoogle}>Sign In</button>
+                            }
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
-            
-     
     )
 }
 
