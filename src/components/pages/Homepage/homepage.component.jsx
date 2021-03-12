9096cba82fd1c97b18f0bde4dd004e73a3b503f3
abhay0480-eng <abhay0480sharma@gmail.com>
2021-03-12 16:56:42 +0530

    adding more files

--- src/components/pages/Homepage/homepage.component.jsx
+++ src/components/pages/Homepage/homepage.component.jsx
@@ -1,33 +1,13 @@
+
 import React from 'react';
 import './homepage.styles.css';
 
-
-
-class Homepage extends React.Component{
-    constructor(){
-        super();
-        this.state={
-
-        }
-    }
-
-    render(){
-        return(
-            <div>
-                <section className="container-fluid">
-                <h1 className="text-center  display-4">Build Your Resume </h1>
-                </section>
-                <section>
-                    <h2>abhay</h2>
-                </section>
-                <section>
-                    <h3> ahsvcjhv</h3>
-                </section>
-
-            </div>
+function Homepage() {
+    return (
+        <div>
             
-        )
-    }
+        </div>
+    )
 }
 
 export default Homepage;
--- src/components/pages/buildResume/buildresume.component.jsx
+++ src/components/pages/buildResume/buildresume.component.jsx
@@ -1,40 +1,20 @@
 import React from 'react';
 import './buildresume.styles.css';
 import fresher from '../../../assets/fresher.jpg';
-import exp from '../../../assets/experience.jpg';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
 import {Link} from 'react-router-dom';
 
 class BuildResume extends React.Component{
     render(){
         return(
-            <Container>
-                <Row>
-                    <Col>
+           
                     <div className="card" >
                 <img class="card-img-top" src={fresher} alt="Card image"></img>
                 <div class="card-body">
-    <h4 class="card-title">For Fresher</h4>
-    <p class="card-text">Build your Resume As a Fresher</p>
-    <Link to='/userform' class="btn btn-primary">click here To Build</Link> 
+    <h4 class="card-title">Build Your Resume Here</h4>
+    <Link to='/userform' class="btn btn-primary">click here </Link> 
     
   </div>
             </div>
-                    </Col>
-                    <Col>
-                    <div className="card" >
-                <img class="card-img-top" src={exp} alt="Card image"></img>
-                <div class="card-body">
-    <h4 class="card-title">For Experience</h4>
-    <p class="card-text">Build your Resume As a Experience</p>
-    <a href="#" class="btn btn-primary">Click Here To Build</a>
-  </div>
-            </div>
-                    </Col>
-                </Row>
-            </Container>
             
         )
     }
