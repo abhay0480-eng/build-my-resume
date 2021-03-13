76662208b4d6ee1d5b1705df7bb0dc51810072e0
abhay0480-eng <abhay0480sharma@gmail.com>
2021-03-13 23:36:55 +0530

    design homepage includes footer main header navigation responsive and add colors theme and social media icons and pictures

--- src/components/pages/Homepage/homepage.component.jsx
+++ src/components/pages/Homepage/homepage.component.jsx
@@ -2,7 +2,14 @@
 import React from 'react';
 import './homepage.styles.css';
 import {Link} from 'react-router-dom';
-import hoepic from '../../../assets/homepic.webp';
+import resumepic from '../../../assets/homepic copy.jpg';
+import image2 from '../../../assets/image2.jpeg';
+import {FaFacebookF } from 'react-icons/fa';
+import {FaGoogle } from 'react-icons/fa';
+import {FaInstagram} from 'react-icons/fa';
+import {FaTwitter} from 'react-icons/fa';
+import {FaLinkedin} from 'react-icons/fa';
+import {FaGithub} from 'react-icons/fa';
 
 function Homepage() {
     
@@ -33,18 +40,54 @@ function Homepage() {
                 </div>
                 <div className="row justify-content-center section-3">
                     <div className="col-12 col-sm-6">
-                    <img src={hoepic} class="img-fluid" alt="resumepic"/>
+                    <img src={resumepic} class="img-fluid img1" alt="resumepic"/>
                     </div>
                 </div>
                 <div className="row justify-content-center section-4">
                     <div className="col-12 align-self-center col-sm-9">
-                    <p className="display-5 text-center">Effortlessly make a job-worthy resume and cover letter that gets you hired faster</p>
+                    <p className="display-6 text-center">Effortlessly make a job-worthy resume and cover letter that gets you hired faster</p>
                     </div>
                 </div>
-
+                <div className="row justify-content-center  section-5">
+                    <div className="col-12 align-self-center  text-center col-sm-6">
+                    <img src={image2} class="img-fluid " width="350px" height="400px"alt="resumepic2"/>
+                    </div>
+                    <div className="col-12 align-self-center text-center col-sm-6">
+                    <p className="h3">Start building your resume today for free!</p>
+                    <Link className="link" to='/userform'>
+                       <div className="col-12 col-sm-3 d-grid gap-2 mx-auto">
+                       <button className="btn btn-secondary" >Build Your Resume Now</button> 
+                       
+                       </div>
+                       </Link>
+                          
+                    </div>
+                </div>
+                
+                
                 
                 
             </div>
+            <footer className="row  justify-content-center">
+                    
+                    <div className="col-sm-12  " >
+                    <p className="text-center">Contact with us on Social Media</p>
+                    <div className="align-items-center justify-content-center d-flex">
+                    <a class=" facebook btn m-1 btn-floating rounded-circle" role="button" href="https://www.facebook.com" ><FaFacebookF color="white"/></a>
+                    <a class=" google btn m-1 btn-floating rounded-circle " href="https://www.google.com" ><FaGoogle color="white"/></a>
+                    <a class=" twitter btn m-1 btn-floating rounded-circle " href="https://www.twitter.com" ><FaTwitter color="white"/></a>
+                    <a class=" instagram btn m-1 btn-floating rounded-circle " href="https://www.instagram.com" ><FaInstagram color="white"/></a>
+                    <a class=" linkedin btn m-1 btn-floating rounded-circle " href="https://www.linkedin.com" ><FaLinkedin color="white"/></a>
+                    <a class=" github btn m-1 btn-floating rounded-circle " href="https://www.github.com" ><FaGithub color="white"/></a>
+                    </div>
+                    </div>
+                    <div className="align-self-center">
+                    <p className="text-center">&copy;2021 copyright-resumebuilder.com</p>
+                    </div>
+                    
+                    
+                </footer>
+           
         </div>
     )
 }
